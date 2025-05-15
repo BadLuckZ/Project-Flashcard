923e8eb9a39bc1da8d55482b7ad704c0c9d7cb9a
BadLuckZ <thanagorn8802@gmail.com>
2025-05-15 12:18:57 +0700

    Add Kanit Font and Background Color

--- src/routes/EntryPage.jsx
+++ src/routes/EntryPage.jsx
@@ -11,7 +11,7 @@ export default function EntryPage() {
     <div className="flex flex-col justify-center items-center gap-16">
       <h1 className="text-bold text-6xl font-bold text-center">My Flashcard</h1>
       <button
-        className="border-2 bg-amber-300 rounded-lg py-2 px-10 cursor-pointer text-2xl"
+        className="border-2 bg-myyellow rounded-lg py-2 px-10 cursor-pointer text-2xl"
         onClick={goToGroupPage}
       >
         Start
