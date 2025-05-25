0ebdc62d1dcf3695ff184545fb795368f0d0377a
BadLuckZ <thanagorn8802@gmail.com>
2025-05-25 14:19:47 +0700

    Add Logo Again

--- src/routes/EntryPage.jsx
+++ src/routes/EntryPage.jsx
@@ -6,9 +6,13 @@ export default function EntryPage() {
 
   return (
     <div className="flex flex-col items-center gap-16 min-h-screen h-fit justify-center py-4">
-      <h1 className="text-5xl font-bold text-center text-myblack sm:text-6xl">
-        My Flashcard
-      </h1>
+      <div className="flex gap-4 items-center justify-center">
+        <img src="/logo.svg" className="w-[50px] h-auto" />
+        <h1 className="text-5xl font-bold text-center text-myblack sm:text-6xl">
+          My Flashcard
+        </h1>
+        <img src="/logo.svg" className="w-[50px] h-auto" />
+      </div>
       <ActionButton
         text={"Start"}
         onClick={() => {
